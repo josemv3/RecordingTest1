54eb0622ed0a12bbf6b5bcaec861f75acc662480
Joey Rubin <josemv3@mac.com>
2023-10-13 08:47:09 -0700

    Lowered target

--- RecordingTest1/AudioManager.swift
+++ RecordingTest1/AudioManager.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import AVFoundation
+
+class AudioManager: ObservableObject {
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    
+    @Published var recordings: [URL] = []
+    
+    func startRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.record, mode: .default)
+            try audioSession.setActive(true)
+            
+            let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let audioURL = documentPath.appendingPathComponent(UUID().uuidString).appendingPathExtension("m4a")
+            
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            audioRecorder.record()
+            
+        } catch {
+            print("Failed to record.")
+        }
+    }
+    
+    func stopRecording() {
+        audioRecorder.stop()
+        recordings.append(audioRecorder.url)
+    }
+    
+    func playAudio(url: URL) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+            print("audio playing", url)
+        } catch {
+            print("Failed to play audio.")
+        }
+    }
+}
+
--- RecordingTest1/ContentView.swift
+++ RecordingTest1/ContentView.swift
@@ -6,17 +6,40 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @StateObject var audioManager = AudioManager()
+        @State private var isRecording = false
+
+        var body: some View {
+            VStack {
+                Button(action: {
+                    if isRecording {
+                        audioManager.stopRecording()
+                    } else {
+                        audioManager.startRecording()
+                    }
+                    
+                    isRecording.toggle()
+                }) {
+                    Text(isRecording ? "Stop Recording" : "Start Recording")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+
+                List(audioManager.recordings, id: \.self) { url in
+                    Button(action: {
+                        audioManager.playAudio(url: url)
+                    }) {
+                        Text(url.lastPathComponent)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
